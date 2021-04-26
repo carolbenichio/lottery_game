3f8a11745a5069c3bf49c6e46fffa70784a1a5bc
Caroline Benichio Teixeira <benichiocarol@gmail.com>
2021-04-26 12:11:19 -0300

    Acertei alguns erros (espacos, quantos e quais numeros acertaram e acrescentei mais comentarios no final)

--- lottery_game.js
+++ lottery_game.js
@@ -1,33 +1,39 @@
 // Lottery game
 
-let lotteryLength = 6;
+let lotteryLength = 3;
 let lotteryNumbers = []; 
 let maxNumber = 60; // In this example, the lottery numbers can go from 1 to 60
-let game = [6, 18, 10, 57, 28, 39];
+let game = [6, 18, 10];
 
 
 // Let the games begin!
 
-// Draw the lottery numbers
+// Draw the lottery number
 for (let index = 0; index < lotteryLength; index +=1) { 
     lotteryNumbers.push(" "+Math.ceil(Math.random() * maxNumber));
 }
 
 // Reads your game and shows how much you got it!
 let hits = 0; // Shows how many times you hit one number
-//let hitNumbers = []; // Shows witch numbers you nailed it
+let hitNumbers = []; // Shows witch numbers you nailed it
 
 for (let index1 = 0; index1 < 6; index1 +=1) {
     for (let index2 = 0; index2 < 6; index2 +=1) {    
-        if (game[index2] === lotteryNumbers[index1]) {
+        if (" "+game[index2] == lotteryNumbers[index1]) {
             hits+=1;
-            //hitNumbers.push(game[index2]);
+            hitNumbers.push(game[index2]);
         }
     }
 }
 // Print the final messages
 console.log("-----------------------------------------------------------------");
-console.log("Os números sorteados são:"+lotteryNumbers+".");
-console.log("O seu jogo é: "+game.join(", ")+".");
-console.log("Você acertou "+hits+" números!");
+console.log("The lottery numbers are:"+lotteryNumbers+".");
+console.log("Your numbers are: "+game.join(", ")+".");
+console.log("You hit "+hits+" number(s)!");
+if (hits !== 0) {
+    console.log("Your hit numbers are: "+hitNumbers.join(", ")+"!");
+    console.log("Congratulations!")
+} else {
+    console.log("Try again! :)");
+}
 console.log("-----------------------------------------------------------------");
